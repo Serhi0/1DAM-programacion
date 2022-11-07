1bb482a3656d4c72caed808f05a86548e2b0b03c
Sergio <albagarbanzo@hotmail.com>
2022-11-07 19:33:41 +0100

    Add files via upload

---
+++ Ejercicios tema 2/Ejercicio7.java
@@ -0,0 +1,12 @@
+/*
+ */
+package mario;
+
+import bpc.daw.mario.Mario;
+import bpc.daw.mario.*;
+
+public class Ejercicio7 {
+    public static void main(String)
+            
+            //nuevo proyecto depositoAgua
+}
