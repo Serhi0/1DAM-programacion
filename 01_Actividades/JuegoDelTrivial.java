b38e3754a4c07b21c8b71f42594bdc0123d85ae7
Sergio <albagarbanzo@hotmail.com>
2022-11-27 18:34:41 +0100

    Create JuegoDelTrivial.java

---
+++ 01_Actividades/JuegoDelTrivial.java
@@ -0,0 +1,423 @@
+package juegodeltrivial;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class JuegoDelTrivial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+        System.out.println("     ____.                                 .___     .__      __         .__      .__       .__    \n" +
+"    |    |__ __   ____   ____   ____     __| _/____ |  |   _/  |________|__|__  _|__|____  |  |   \n" +
+"    |    |  |  \\_/ __ \\ / ___\\ /  _ \\   / __ |/ __ \\|  |   \\   __\\_  __ \\  \\  \\/ /  \\__  \\ |  |   \n" +
+"/\\__|    |  |  /\\  ___// /_/  >  <_> ) / /_/ \\  ___/|  |__  |  |  |  | \\/  |\\   /|  |/ __ \\|  |__ \n" +
+"\\________|____/  \\___  >___  / \\____/  \\____ |\\___  >____/  |__|  |__|  |__| \\_/ |__(____  /____/ \n" +
+"                     \\/_____/               \\/    \\/                                     \\/     ");
+        
+        System.out.println("================================================================================================");
+        System.out.println("=          \u269C                  Juego del trivial de cultura general         \u269C                   =");
+        System.out.println("================================================================================================");
+        
+        System.out.println("Escriba su nombre: ");
+        String nombre = new Scanner(System.in).nextLine();
+        
+        //System.out.println(respuesta.equals(solucion));
+        
+        String preguntas[] ={"¿Helsinki es la capital de qué país?","¿Cómo se llama la línea vertical imaginaria a partir de la cual se miden las longitudes?",
+                             "¿En cuál planeta nació el personaje ficticio Superman?","¿En qué año se hundió el Titanic?",
+                             "¿Cuál era el segundo nombre de Michael Jackson?","¿En qué país se habla mayoritariamente el idioma tagálog?",
+                             "¿Cuál es la hormona que provoca el estrés?","¿Cómo se llama la moneda oficial de Japón?","¿Cuál es la isla más grande del Mar Caribe?",
+                             "¿Apellido de la primera mujer en ganar un Premio Nobel?","¿Cuál es el único alimento comestible que nunca se daña?"};
+        
+        String respuestas[]={"Finlandia","Greenwich","Krypton","1912","Joseph","Filipinas","Cortisol","Yen","Cuba","Curie","Miel"};
+        
+        String ranking[]={"¡Necesitas leer un poco!","¡Suspenso!","¡Aprobado justo!","¡Bien hecho!","¡PERFECT!"};
+                
+        int numPregunta=1;
+        int puntuacion=0;
+        int numero = (int)(Math.random()*preguntas.length);
+        String solucion;
+        int preguntaLibre[]={1,1,1,1,1,1,1,1,1,1,1};
+        
+        System.out.println("Pregunta número: "+numPregunta);
+        numPregunta++;
+        System.out.println(preguntas[numero]);
+        
+        solucion = new Scanner(System.in).nextLine();
+        
+        if (respuestas[numero].equalsIgnoreCase(solucion.replaceAll("\\s+",""))){
+            System.out.println("\u001B[32m"+"Bien"+"\u001B[0m");
+                    puntuacion++;
+        }else{
+            System.out.println("\u001B[31m"+"Mal"+"\u001B[0m");
+        }
+        
+        preguntaLibre[numero]=0;
+        numero = (int)(Math.random()*preguntas.length);
+        
+        if(preguntaLibre[numero] == 0){
+            if(preguntaLibre[numero] != preguntaLibre[0]){
+               numero=0;
+            }else{
+                if(preguntaLibre[numero] != preguntaLibre[1]){
+                   numero=1;
+                }else{
+                    if(preguntaLibre[numero] != preguntaLibre[2]){
+                       numero=2;
+                    }else{
+                        if(preguntaLibre[numero] != preguntaLibre[3]){
+                           numero=3;
+                        }else{
+                            if(preguntaLibre[numero] != preguntaLibre[4]){ 
+                               numero=4;
+                            }else{
+                                if(preguntaLibre[numero] != preguntaLibre[5]){
+                                   numero=5;
+                                }else{
+                                    if(preguntaLibre[numero] != preguntaLibre[6]){
+                                       numero=6;
+                                    }else{
+                                        if(preguntaLibre[numero] != preguntaLibre[7]){
+                                           numero=7;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }   
+        
+        System.out.println("Pregunta número: "+numPregunta);
+        numPregunta++;
+        System.out.println(preguntas[numero]);solucion = new Scanner(System.in).nextLine();
+        
+       if (respuestas[numero].equalsIgnoreCase(solucion.replaceAll("\\s+",""))){
+            System.out.println("\u001B[32m"+"Bien"+"\u001B[0m");
+                    puntuacion++;
+        }else{
+            System.out.println("\u001B[31m"+"Mal"+"\u001B[0m");
+        }
+        
+        preguntaLibre[numero]=0;
+        numero = (int)(Math.random()*preguntas.length);
+
+        if(preguntaLibre[numero] == 0){
+            if(preguntaLibre[numero] != preguntaLibre[0]){
+               numero=0;
+            }else{
+                if(preguntaLibre[numero] != preguntaLibre[1]){
+                   numero=1;
+                }else{
+                    if(preguntaLibre[numero] != preguntaLibre[2]){
+                       numero=2;
+                    }else{
+                        if(preguntaLibre[numero] != preguntaLibre[3]){
+                           numero=3;
+                        }else{
+                            if(preguntaLibre[numero] != preguntaLibre[4]){ 
+                               numero=4;
+                            }else{
+                                if(preguntaLibre[numero] != preguntaLibre[5]){
+                                   numero=5;
+                                }else{
+                                    if(preguntaLibre[numero] != preguntaLibre[6]){
+                                       numero=6;
+                                    }else{
+                                        if(preguntaLibre[numero] != preguntaLibre[7]){
+                                           numero=7;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }   
+        
+        System.out.println("Pregunta número: "+numPregunta);
+        numPregunta++;
+        System.out.println(preguntas[numero]);solucion = new Scanner(System.in).nextLine();
+        
+        if (respuestas[numero].equalsIgnoreCase(solucion.replaceAll("\\s+",""))){
+            System.out.println("\u001B[32m"+"Bien"+"\u001B[0m");
+                    puntuacion++;
+        }else{
+            System.out.println("\u001B[31m"+"Mal"+"\u001B[0m");
+        }
+        
+        preguntaLibre[numero]=0;
+        numero = (int)(Math.random()*preguntas.length);
+       
+        
+        if(preguntaLibre[numero] == 0){
+            if(preguntaLibre[numero] != preguntaLibre[0]){
+               numero=0;
+            }else{
+                if(preguntaLibre[numero] != preguntaLibre[1]){
+                   numero=1;
+                }else{
+                    if(preguntaLibre[numero] != preguntaLibre[2]){
+                       numero=2;
+                    }else{
+                        if(preguntaLibre[numero] != preguntaLibre[3]){
+                           numero=3;
+                        }else{
+                            if(preguntaLibre[numero] != preguntaLibre[4]){ 
+                               numero=4;
+                            }else{
+                                if(preguntaLibre[numero] != preguntaLibre[5]){
+                                   numero=5;
+                                }else{
+                                    if(preguntaLibre[numero] != preguntaLibre[6]){
+                                       numero=6;
+                                    }else{
+                                        if(preguntaLibre[numero] != preguntaLibre[7]){
+                                           numero=7;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }   
+            
+        System.out.println("Pregunta número: "+numPregunta);
+        numPregunta++;
+        System.out.println(preguntas[numero]);solucion = new Scanner(System.in).nextLine();
+        
+        if (respuestas[numero].equalsIgnoreCase(solucion.replaceAll("\\s+",""))){
+            System.out.println("\u001B[32m"+"Bien"+"\u001B[0m");
+                    puntuacion++;
+        }else{
+            System.out.println("\u001B[31m"+"Mal"+"\u001B[0m");
+        }
+        
+        preguntaLibre[numero]=0;
+        numero = (int)(Math.random()*preguntas.length);
+
+        
+        if(preguntaLibre[numero] == 0){
+            if(preguntaLibre[numero] != preguntaLibre[0]){
+               numero=0;
+            }else{
+                if(preguntaLibre[numero] != preguntaLibre[1]){
+                   numero=1;
+                }else{
+                    if(preguntaLibre[numero] != preguntaLibre[2]){
+                       numero=2;
+                    }else{
+                        if(preguntaLibre[numero] != preguntaLibre[3]){
+                           numero=3;
+                        }else{
+                            if(preguntaLibre[numero] != preguntaLibre[4]){ 
+                               numero=4;
+                            }else{
+                                if(preguntaLibre[numero] != preguntaLibre[5]){
+                                   numero=5;
+                                }else{
+                                    if(preguntaLibre[numero] != preguntaLibre[6]){
+                                       numero=6;
+                                    }else{
+                                        if(preguntaLibre[numero] != preguntaLibre[7]){
+                                           numero=7;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }   
+        
+        System.out.println("Pregunta número: "+numPregunta);
+        numPregunta++;
+        System.out.println(preguntas[numero]);solucion = new Scanner(System.in).nextLine();
+        
+        if (respuestas[numero].equalsIgnoreCase(solucion.replaceAll("\\s+",""))){
+            System.out.println("\u001B[32m"+"Bien"+"\u001B[0m");
+                    puntuacion++;
+        }else{
+            System.out.println("\u001B[31m"+"Mal"+"\u001B[0m");
+        }
+        
+        preguntaLibre[numero]=0;
+        numero = (int)(Math.random()*preguntas.length);
+        
+        if(preguntaLibre[numero] == 0){
+            if(preguntaLibre[numero] != preguntaLibre[0]){
+               numero=0;
+            }else{
+                if(preguntaLibre[numero] != preguntaLibre[1]){
+                   numero=1;
+                }else{
+                    if(preguntaLibre[numero] != preguntaLibre[2]){
+                       numero=2;
+                    }else{
+                        if(preguntaLibre[numero] != preguntaLibre[3]){
+                           numero=3;
+                        }else{
+                            if(preguntaLibre[numero] != preguntaLibre[4]){ 
+                               numero=4;
+                            }else{
+                                if(preguntaLibre[numero] != preguntaLibre[5]){
+                                   numero=5;
+                                }else{
+                                    if(preguntaLibre[numero] != preguntaLibre[6]){
+                                       numero=6;
+                                    }else{
+                                        if(preguntaLibre[numero] != preguntaLibre[7]){
+                                           numero=7;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }   
+   
+        System.out.println("Pregunta número: "+numPregunta);
+        numPregunta++;
+        System.out.println(preguntas[numero]);solucion = new Scanner(System.in).nextLine();
+        
+        if (respuestas[numero].equalsIgnoreCase(solucion.replaceAll("\\s+",""))){
+            System.out.println("\u001B[32m"+"Bien"+"\u001B[0m");
+                    puntuacion++;
+        }else{
+            System.out.println("\u001B[31m"+"Mal"+"\u001B[0m");
+        }
+        
+        preguntaLibre[numero]=0;
+        numero = (int)(Math.random()*preguntas.length);
+        
+        if(preguntaLibre[numero] == 0){
+            if(preguntaLibre[numero] != preguntaLibre[0]){
+               numero=0;
+            }else{
+                if(preguntaLibre[numero] != preguntaLibre[1]){
+                   numero=1;
+                }else{
+                    if(preguntaLibre[numero] != preguntaLibre[2]){
+                       numero=2;
+                    }else{
+                        if(preguntaLibre[numero] != preguntaLibre[3]){
+                           numero=3;
+                        }else{
+                            if(preguntaLibre[numero] != preguntaLibre[4]){ 
+                               numero=4;
+                            }else{
+                                if(preguntaLibre[numero] != preguntaLibre[5]){
+                                   numero=5;
+                                }else{
+                                    if(preguntaLibre[numero] != preguntaLibre[6]){
+                                       numero=6;
+                                    }else{
+                                        if(preguntaLibre[numero] != preguntaLibre[7]){
+                                           numero=7;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+        
+        System.out.println("Pregunta número: "+numPregunta);
+        numPregunta++;
+        System.out.println(preguntas[numero]);solucion = new Scanner(System.in).nextLine();
+        
+        if (respuestas[numero].equalsIgnoreCase(solucion.replaceAll("\\s+",""))){
+            System.out.println("\u001B[32m"+"Bien"+"\u001B[0m");
+                    puntuacion++;
+        }else{
+            System.out.println("\u001B[31m"+"Mal"+"\u001B[0m");
+        }
+        
+        preguntaLibre[numero]=0;
+        numero = (int)(Math.random()*preguntas.length);
+
+        
+        if(preguntaLibre[numero] == 0){
+            if(preguntaLibre[numero] != preguntaLibre[0]){
+               numero=0;
+            }else{
+                if(preguntaLibre[numero] != preguntaLibre[1]){
+                   numero=1;
+                }else{
+                    if(preguntaLibre[numero] != preguntaLibre[2]){
+                       numero=2;
+                    }else{
+                        if(preguntaLibre[numero] != preguntaLibre[3]){
+                           numero=3;
+                        }else{
+                            if(preguntaLibre[numero] != preguntaLibre[4]){ 
+                               numero=4;
+                            }else{
+                                if(preguntaLibre[numero] != preguntaLibre[5]){
+                                   numero=5;
+                                }else{
+                                    if(preguntaLibre[numero] != preguntaLibre[6]){
+                                       numero=6;
+                                    }else{
+                                        if(preguntaLibre[numero] != preguntaLibre[7]){
+                                           numero=7;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }   
+
+        System.out.println("Pregunta número: "+numPregunta);
+        numPregunta++;
+        System.out.println(preguntas[numero]);solucion = new Scanner(System.in).nextLine();
+        
+
+        if (respuestas[numero].equalsIgnoreCase(solucion.replaceAll("\\s+",""))){
+            System.out.println("\u001B[32m"+"Bien"+"\u001B[0m");
+                    puntuacion++;
+        }else{
+            System.out.println("\u001B[31m"+"Mal"+"\u001B[0m");
+        }
+        
+            System.out.println("\n"+nombre+", Tu puntuación es: "+puntuacion);
+            
+            if(puntuacion== 0){
+                System.out.println("\u001B[31m"+ranking[0]+"\u001B[0m");
+            }else{
+                if(puntuacion >0 && puntuacion <4){
+                   System.out.println("\u001B[31m"+ranking[1]+"\u001B[0m");
+                }else{
+                    if(puntuacion == 4){
+                       System.out.println("\u001B[33m"+ranking[2]+"\u001B[0m");
+                    }else{
+                        if(puntuacion >=5 && puntuacion <8){
+                           System.out.println("\u001B[32m"+ranking[3]+"\u001B[0m"); 
+                        }else{
+                            System.out.println("\u001B[35m"+ranking[4]+"\u001B[0m");
+                        }    
+                    }        
+                }            
+            }            
+                    
+                
+    }
+}
