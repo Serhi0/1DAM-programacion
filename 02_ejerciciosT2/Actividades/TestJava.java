db7e3d41bd1633db1e2524c1f73be11c1b202c3c
Sergio <albagarbanzo@hotmail.com>
2022-11-27 21:56:33 +0100

    Update TestJava.java

--- 02_ejerciciosT2/Actividades/TestJava.java
+++ 02_ejerciciosT2/Actividades/TestJava.java
@@ -8,33 +8,35 @@ public class TestJava {
 
     
     public static void main(String[] args) {
+        //Propiedades
         String nombreSO = "os.name";                 //Nombre del sistema operativo
-        String versionSO = "os.version";             //Versi髇 del sistema operativo
+        String versionSO = "os.version";             //Versi贸n del sistema operativo
         String arquitecturaSO = "os.arch";           //Arquitectura del sistema operativo
-        String file_separator = "file.separator";    //Car醕ter que separa los componentes de una ruta de acceso de archivo. Esto es "" en UNIX y "" en Windows./\
-        String java_class_path= "java.class.path";   //Ruta utilizada para buscar directorios y archivos JAR que contienen archivos de clase. Los elementos de la ruta de acceso de clase est醤 separados por un car醕ter espec韋ico de la plataforma especificado en la propiedad.path.separator
-        String java_home = "java.home";              //Directorio de instalaci髇 para Java Runtime Environment (JRE)
+        String file_separator = "file.separator";    //Car谩cter que separa los componentes de una ruta de acceso de archivo. Esto es "" en UNIX y "" en Windows./\
+        String java_class_path= "java.class.path";   //Ruta utilizada para buscar directorios y archivos JAR que contienen archivos de clase. Los elementos de la ruta de acceso de clase est谩n separados por un car谩cter espec铆fico de la plataforma especificado en la propiedad.path.separator
+        String java_home = "java.home";              //Directorio de instalaci贸n para Java Runtime Environment (JRE)
         String java_vendor = "java.vendor";          //Nombre del proveedor de JRE
         String java_vendor_url = "java.vendor.url";  //URL del proveedor de JRE
-        String java_version = "java.version";        //N鷐ero de versi髇 de JRE
-        String line_separator = "line.separator";    //Secuencia utilizada por el sistema operativo para separar l韓eas en archivos de texto
-        String path_separator = "path.separator";    //Car醕ter separador de trazado utilizado enjava.class.path
+        String java_version = "java.version";        //N煤mero de versi贸n de JRE
+        String line_separator = "line.separator";    //Secuencia utilizada por el sistema operativo para separar l铆neas en archivos de texto
+        String path_separator = "path.separator";    //Car谩cter separador de trazado utilizado enjava.class.path
         String user_dir = "user.dir";                //Directorio de trabajo de usuarios
         String user_home = "user.home";              //Directorio de inicio de usuario
         String user_name = "user.name";              //Nombre de cuenta de usuario
         
+        //System.getProperty  devuelve un string que contiene el valor de la propiedad
         System.out.println("=============================");
-        System.out.println("= Informaci髇 sobre el S.O. =");
+        System.out.println("= Informaci贸n sobre el S.O. =");
         System.out.println("=============================");
-        System.out.println("Nombre del S.O.: "+ System.getProperty(nombreSO));
-        System.out.println("Versi髇 del S.O.: " + System.getProperty(versionSO));
+        System.out.println("Nombre del S.O.: "+ System.getProperty(nombreSO));   
+        System.out.println("Versi贸n del S.O.: " + System.getProperty(versionSO));
         System.out.println("Arquitectura del S.O.: "+System.getProperty(arquitecturaSO));
         System.out.println("File separator: "+System.getProperty(file_separator));
         System.out.println("Java class path: "+System.getProperty(java_class_path));
         System.out.println("Java home: "+System.getProperty(java_home));
         System.out.println("Java vendor: "+System.getProperty(java_vendor));
         System.out.println("Java vendor url: "+System.getProperty(java_vendor_url));
-        System.out.println("Java versi髇: "+System.getProperty(java_version));
+        System.out.println("Java versi贸n: "+System.getProperty(java_version));
         System.out.println("Line separator: "+System.getProperty(line_separator));
         System.out.println("Path separator: "+System.getProperty(path_separator));
         System.out.println("User dir: "+System.getProperty(user_dir));
