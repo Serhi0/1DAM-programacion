4f21f272607ffaa27ac74a09a2ce7f898e89e07c
Sergio <serhi0.oficial@gmail.com>
2023-02-03 18:20:56 +0100

    e10

--- 04_ejerciciosT4/clases/Persona.java
+++ 04_ejerciciosT4/clases/Persona.java
@@ -1,8 +1,8 @@
-
-
 package clases;
 import clases.DNI;
 import java.time.*;
+import clases.CuentaCorriente;
+
 /**
  *
  * @author Sergio serhi0.oficial@gmail.com
@@ -13,24 +13,35 @@ public class Persona {
     private DNI dni;
     private double sueldo;
     private LocalDate fechaNacimiento;
+    private CuentaCorriente cuenta;
     
     public Persona(String n,DNI d,double s, LocalDate fn){
         nombre = n;
         dni = d;
         sueldo = s;
         fechaNacimiento = fn;
+        this.cuenta = cuenta = new CuentaCorriente();
+        
     }
     public Persona(String n, int numDNI, char letraDNI, double s, LocalDate fn){
         nombre = n;
         dni = new DNI(numDNI,letraDNI);
         sueldo = s;
-        fechaNacimiento = fn;       
+        fechaNacimiento = fn;  
+        this.cuenta = cuenta = new CuentaCorriente();
     }
     public Persona(String n,DNI d){
         nombre = n;
         dni = d;
         sueldo = 900;
         fechaNacimiento = LocalDate.now().minusYears(20);
+        this.cuenta = cuenta = new CuentaCorriente();
+    }
+    public void aumentarSueldo(int porcentaje){
+        sueldo %= porcentaje;  
+    }
+    public void cobrarSueldo(){
+        this.cuenta.añadirDinero((int) sueldo);
     }
     
 
